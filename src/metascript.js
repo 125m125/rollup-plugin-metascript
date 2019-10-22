54f667e46e041ff232c02525921196334bb7cb33
125m125 <125m125@125m125.de>
2019-10-22 21:59:51 +0200

    add debug logging

--- src/metascript.js
+++ src/metascript.js
@@ -6,18 +6,34 @@ import MetaScript from "metascript";
 export default function metascript(options = {}) {
     var filter = createFilter(options.include, options.exclude);
     var scope = options.scope || {};
-
+    var logger = options.logger;
 
     return {
         name: "metascript",
         transform(code, id) {
             if (filter(id)) {
+                var partiallyAppliedTransform = transform.bind(this, logger, code, id);
                 if (scope && typeof scope.then === "function") {
-                    return scope.then(realScope => MetaScript.transform(code, id, realScope));
+                    return scope.then(partiallyAppliedTransform);
                 } else {
-                    return MetaScript.transform(code, id, scope);
+                    return partiallyAppliedTransform(scope);
                 }
             }
         },
     };
+}
+
+function transform(logger, code, id, scope) {
+    scope = typeof scope === "function" ? scope() : scope;
+    log(logger, "input: ", code.replace(/\r?\n|\r/g, "\u23CE"));
+    log(logger, "scope: ", JSON.stringify(scope));
+    var result = MetaScript.transform(code, id, scope);
+    log(logger, "outpt:", result.replace(/\r?\n|\r/g, "\u23CE"));
+    return result;
+}
+
+function log(logger, ...message) {
+    if (typeof logger == "undefined" || !logger) return;
+    var call = (typeof logger === "function") ? logger : logger.debug || logger.log;
+    if (call) call(...message);
 }
